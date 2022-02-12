2b8a6252d670186260a3f4cdd4261a777979c758
Lucas Barga <lucasmpbarga@gmail.com>
2022-02-12 12:11:16 -0300

    implement first screen

---
+++ lib/constants/constants.dart
@@ -0,0 +1,3 @@
+const String mak3rs = "Mak3rs";
+const String yourIdea = "sua ideia. nosso trabalho.";
+const String start = "começar";
---
+++ lib/constants/palette.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color white = Color(0xffffffff);
+const Color black = Colors.black;
+const Color grey = Color(0xff333333);
